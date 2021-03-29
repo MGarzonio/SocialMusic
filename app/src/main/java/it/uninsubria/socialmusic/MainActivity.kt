c5d803749888691000401e4716c9b989ed932f26
Mattia Garzonio <mat.garzonio@gmail.com>
2021-03-29 17:05:24 +0200

    restore default

--- app/src/main/java/it/uninsubria/socialmusic/MainActivity.kt
+++ app/src/main/java/it/uninsubria/socialmusic/MainActivity.kt
@@ -7,6 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //push di prova cambiata apposta per fare un altra prova di questo bellissimo sistema
     }
 }
