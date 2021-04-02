739018ca835fa975f785d2e387503e5c7d32f751
Mattia Garzonio <mat.garzonio@gmail.com>
2021-04-02 17:13:14 +0200

    first MainActivity Layout open new Sing In Activity on click Sing In button

--- app/src/main/java/it/uninsubria/socialmusic/MainActivity.kt
+++ app/src/main/java/it/uninsubria/socialmusic/MainActivity.kt
@@ -1,11 +1,37 @@
 package it.uninsubria.socialmusic
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
         setContentView(R.layout.activity_main)
+    }
+
+    fun doLogin(m:String, p:String):Boolean
+    {
+        /*
+            LOGIN ACCOUNT FIREBASE
+        */
+        return true;
+    }
+
+    fun openSingIn(view: View) {
+        var mail = editTextUsername.text.toString()
+        var password = editTextPassword.text.toString()
+        if(doLogin(mail, password)) {
+            val intent: Intent = Intent(this, SingInActivity::class.java)
+            intent.putExtra("mail", mail)
+            startActivity(intent)
+        }
     }
 }
