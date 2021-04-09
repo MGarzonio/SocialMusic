dec8857c3805058471734166398c198571262a8e
Mattia Garzonio <mat.garzonio@gmail.com>
2021-04-09 15:55:55 +0200

    added mail and password check

--- app/src/main/java/it/uninsubria/socialmusic/ForgotActivity.kt
+++ app/src/main/java/it/uninsubria/socialmusic/ForgotActivity.kt
@@ -1,11 +1,51 @@
 package it.uninsubria.socialmusic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import java.util.regex.Pattern
 
 class ForgotActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot)
+    }
+
+    private fun isValidPassword(psw: String): Boolean {
+        return psw != null && psw.length >= 4
+    }
+    private fun isValidEmail(email: String): Boolean {
+        val emailPattern = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+        val pattern = Pattern.compile(emailPattern)
+        val matcher = pattern.matcher(email)
+        return matcher.matches()
+    }
+
+    private fun sendRecoveryMail(email :String) :Boolean {
+        /*
+
+            TODO Recovery password
+
+         */
+        return true
+    }
+
+    fun openMain(view: View) {
+        val mail = findViewById<EditText>(R.id.editTextEmail)
+        var checkError = false
+        if (!isValidEmail(mail.text.toString())) {
+            mail.error = getString(R.string.mail_error)
+            checkError = true
+        }
+        if(sendRecoveryMail(mail.text.toString()) && !checkError) {
+            Toast.makeText(applicationContext, getString(R.string.mail_sent), Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        } else {
+            Toast.makeText(applicationContext, getString(R.string.account_not_found), Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/it/uninsubria/socialmusic/MainActivity.kt
+++ app/src/main/java/it/uninsubria/socialmusic/MainActivity.kt
@@ -3,8 +3,11 @@ package it.uninsubria.socialmusic
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,21 +15,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    private fun isValidPassword(psw: String): Boolean {
+        return psw != null && psw.length >= 8
+    }
+    private fun isValidEmail(email: String): Boolean {
+        val emailPattern = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+        val pattern = Pattern.compile(emailPattern)
+        val matcher = pattern.matcher(email)
+        return matcher.matches()
+    }
+
     private fun doLogin(m: String, p: String):Boolean {
         /*
-            LOGIN ACCOUNT FIREBASE
+            TODO login account
         */
         return true
     }
 
     fun openLogin(view: View) {
-        val mail = editTextUsername.text.toString()
-        val password = editTextPassword.text.toString()
-        //if(doLogin(mail, password)) {
+        val mail = findViewById<EditText>(R.id.editTextUsername)
+        val password = findViewById<EditText>(R.id.editTextPassword)
+        var errorCheck = false
+        if(!isValidEmail(mail.text.toString())){
+            mail.error = getString(R.string.mail_error)
+            errorCheck = true
+        }
+        if(!isValidPassword(password.text.toString())){
+            password.error = getString(R.string.password_error)
+        }
+        if(doLogin(mail.text.toString(), password.text.toString()) && !errorCheck) {
             val intent = Intent(this,HomeActivity::class.java )
             //intent.putExtra("mail", mail)
             startActivity(intent)
-        //}
+        } else {
+            Toast.makeText(applicationContext,getString(R.string.invalid_account), Toast.LENGTH_LONG).show()
+        }
     }
 
     fun openSingIn(view: View) {
