44ab686b1819e4f437bfcbfede58b7cbdd2d928d
SPIDERMAN45 <chry45@gmail.com>
2021-04-17 13:03:01 +0200

    drawable, strings and colors

--- app/src/main/java/it/uninsubria/socialmusic/SignUpActivity.kt
+++ app/src/main/java/it/uninsubria/socialmusic/SignUpActivity.kt
@@ -10,7 +10,7 @@ class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sing_in)
+        setContentView(R.layout.activity_sign_up)
 
     }
 
