3c63e3e5163ae5a65dcdb3050d910a03b80615f1
SPIDERMAN45 <chry45@gmail.com>
2021-04-17 15:41:52 +0200

    sign in and sign up check

--- app/src/main/java/it/uninsubria/socialmusic/home/HomeActivity.kt
+++ app/src/main/java/it/uninsubria/socialmusic/home/HomeActivity.kt
@@ -1,10 +1,13 @@
 package it.uninsubria.socialmusic.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import it.uninsubria.socialmusic.LoginActivity
 import it.uninsubria.socialmusic.R
 import it.uninsubria.socialmusic.R.layout.activity_home
 
@@ -13,10 +16,18 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_home)
-
+        verifyUserLoggedIn()
         val controllerNavigation = findNavController(R.id.fragmentView)
         val bottomView = findViewById<BottomNavigationView>(R.id.BottomMenu)
 
         bottomView.setupWithNavController(controllerNavigation)
+    }
+    private fun verifyUserLoggedIn() {
+        val uid = FirebaseAuth.getInstance().uid
+        if(uid == null){
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
     }
 }
