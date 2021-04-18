128b4416b7f952057f3704937aa23a47f3f51bee
Mattia Garzonio <mat.garzonio@gmail.com>
2021-04-18 22:45:05 +0200

    fixed maps activity bug

--- app/src/main/java/it/uninsubria/socialmusic/ListActivity.kt
+++ app/src/main/java/it/uninsubria/socialmusic/ListActivity.kt
@@ -2,10 +2,20 @@ package it.uninsubria.socialmusic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import kotlinx.android.synthetic.main.activity_list.*
 
 class ListActivity : AppCompatActivity() {
+
+    private var adapter = GroupAdapter<GroupieViewHolder>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+        listView.adapter = adapter
+
+
     }
+
 }
--- app/src/main/java/it/uninsubria/socialmusic/recycleviewRow.kt
+++
@@ -1,4 +0,0 @@
-package it.uninsubria.socialmusic
-
-class recycleviewRow {
-}
