77d3340882a0dec9c06eef6498a49cc070777fcb
Mattia Garzonio <mat.garzonio@gmail.com>
2021-04-22 23:13:56 +0200

    added on item selected listener spinner in SearchFragment.kt

--- app/src/main/java/it/uninsubria/socialmusic/home/SearchFragment.kt
+++ app/src/main/java/it/uninsubria/socialmusic/home/SearchFragment.kt
@@ -22,14 +22,12 @@ import it.uninsubria.socialmusic.User
 import it.uninsubria.socialmusic.chat.ChatActivity
 import kotlinx.android.synthetic.main.user_row.view.*
 
-class SearchFragment : Fragment(), View.OnClickListener {
+class SearchFragment : Fragment(), View.OnClickListener{
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var nameKey: EditText
     private lateinit var instrumentKey: Spinner
     private lateinit var genreKey: Spinner
-    private lateinit var instrumentAdapter: ArrayAdapter<String>
-    private lateinit var genresAdapter: ArrayAdapter<String>
 
     val defaultID = "6N9HD0c5WgPsakocjfluSiSI0hm2"
 
@@ -37,19 +35,38 @@ class SearchFragment : Fragment(), View.OnClickListener {
 
         val viewVal = inflater.inflate(R.layout.fragment_search, container, false) as View
         val btnSearch = viewVal.findViewById(R.id.search_button_search) as Button
+        val instrumentTextView = viewVal.findViewById(R.id.instruments_textView_search) as TextView
         nameKey = viewVal.findViewById(R.id.name_editText_search) as EditText
         instrumentKey = viewVal.findViewById(R.id.instrument_Spinner_search) as Spinner
         genreKey = viewVal.findViewById(R.id.genre_Spinner_search) as Spinner
         recyclerView = viewVal.findViewById(R.id.user_recyclerView_search) as RecyclerView
 
-        instrumentAdapter = ArrayAdapter<String>(viewVal.context, android.R.layout.simple_spinner_item, getInstruments())
-        genresAdapter = ArrayAdapter<String>(viewVal.context, android.R.layout.simple_spinner_item, getGenres())
+        val instrumentAdapter = ArrayAdapter(viewVal.context, android.R.layout.simple_spinner_item, getInstruments())
         instrumentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val genresAdapter = ArrayAdapter(viewVal.context, android.R.layout.simple_spinner_item, getGenres())
         genresAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
         instrumentKey.adapter = instrumentAdapter
         genreKey.adapter = genresAdapter
 
         btnSearch.setOnClickListener(this)
+
+        instrumentKey?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                instrumentTextView.text = getString(R.string.musical_instruments)
+            }
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                instrumentTextView.text = instrumentKey.selectedItem.toString()
+            }
+        }
+        genreKey?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+               // genreTextView.text = getString(R.string.musical_genres)
+            }
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+               // genreTextView.text = instrumentKey.selectedItem.toString()
+            }
+        }
 
         fetchUsers(viewVal)
 
@@ -61,7 +78,6 @@ class SearchFragment : Fragment(), View.OnClickListener {
             R.id.search_button_search -> doSearch()
         }
     }
-
 
     private fun getInstruments(): ArrayList<String> {
         var list = ArrayList<String>()
