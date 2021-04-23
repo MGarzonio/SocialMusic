04b3d7f5a5854c7ae1dd15d3884d7aeb014686f9
Mattia Garzonio <mat.garzonio@gmail.com>
2021-04-23 16:40:14 +0200

    implemented name search query

--- app/src/main/java/it/uninsubria/socialmusic/home/SearchFragment.kt
+++ app/src/main/java/it/uninsubria/socialmusic/home/SearchFragment.kt
@@ -9,10 +9,9 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
@@ -71,14 +70,12 @@ class SearchFragment : Fragment(), View.OnClickListener{
             }
         }
 
-        fetchUsers(viewVal)
-
         return viewVal
     }
 
     override fun onClick(v: View) {
         when (v.id) {
-            R.id.search_button_search -> doSearch()
+            R.id.search_button_search -> doSearch(v)
         }
     }
 
@@ -104,7 +101,7 @@ class SearchFragment : Fragment(), View.OnClickListener{
         return list
     }
 
-    private fun fetchUsers(view: View) {
+    private fun fetchUsers(view: View, selectedName : String) {
         val ref = FirebaseDatabase.getInstance().getReference("/users")
         val myUid = FirebaseAuth.getInstance().uid
         ref.addListenerForSingleValueEvent(object : ValueEventListener {
@@ -113,7 +110,11 @@ class SearchFragment : Fragment(), View.OnClickListener{
                 snapshot.children.forEach {
                     val user = it.getValue(User::class.java)
                     if (user != null && user.uid != myUid && user.uid != defaultID) {
-                        adapter.add(UserItem(user))
+                        if(selectedName != ""){
+                            if(user.name == selectedName || user.surname == selectedName || user.username == selectedName)
+                                adapter.add(UserItem(user))
+                        } else
+                            adapter.add(UserItem(user))
                     }
                 }
                 adapter.setOnItemClickListener { item, view ->
@@ -131,8 +132,8 @@ class SearchFragment : Fragment(), View.OnClickListener{
         })
     }
 
-    private fun doSearch() {
-        // TODO(do research with filters on firebase)
+    private fun doSearch(view: View) {
+        fetchUsers(view, nameKey.text.toString())
     }
 
     class UserItem(val user: User) : Item<GroupieViewHolder>() {
